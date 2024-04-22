d840f81dac585e4e778bbd7e8555772dd5a1298f
Marcelo Fassbinder Martins <mfassbin@c1r7s4.42porto.com>
2024-04-22 19:48:04 +0100

    working without times_to_eat

--- input.c
+++ input.c
@@ -6,7 +6,7 @@
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:37:30 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/04/16 15:53:05 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/04/22 16:59:35 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,12 @@ int	check_input(int argc, char **argv)
 		|| !arg_is_digit(argv[3]) || !arg_is_digit(argv[4])
 			|| (argv[5] && !arg_is_digit(argv[5])))
 	{
-		write(2, "Error!\nOne or more parameters are not valid.\n", 47);
+		write(2, "Error!\nOne or more parameters are not valid.\n", 46);
+		return (0);
+	}
+	if (ft_atoi(argv[1]) <= 0)
+	{
+		write(2, "Error!\nThe number of philosophers is not valid.\n", 48);
 		return (0);
 	}
 	return (1);
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 11:26:16 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/04/20 19:42:15 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/04/22 19:19:52 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,23 @@
 #include <sys/time.h>
 #include <stdbool.h>
 
+// * * * COLORS * * *
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+# define RESET "\033[0m"
+
+
 // * * * STRUCUTURE * * *
 
 typedef struct 		s_philo
 {
 	int					id;
-	int					is_dead;
 	size_t				last_meal;
 	pthread_t			thread;
 	pthread_mutex_t		*l_fork;
@@ -41,21 +52,30 @@ typedef struct		s_program
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				times_must_eat;
-	pthread_mutex_t	*forks;
-	t_philo			*philos;
+	bool			is_dead;
 	bool			threads_ready;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print;
+	pthread_mutex_t	monitor;
+	pthread_mutex_t	death;
+	t_philo			*philos;
 }					t_program;
 
 
 void	*routine(void *philo);
 int monitoring(t_program *prog);
-size_t	ph_is_eating(int id, size_t start, pthread_mutex_t *first_fork, pthread_mutex_t *second_fork, int time_to_eat);
-int init_forks(t_program *prog);
+void	print_action(t_philo *philo, char *msg, char *color);
+void	ph_is_eating(t_philo *ph, size_t start, pthread_mutex_t *first_fork, pthread_mutex_t *second_fork);
+int init_mutexes(t_program *prog);
 int init_philos(t_program *prog);
 int	init_program(char **argv, t_program *prog);
 int	ft_atoi(char *str);
 int check_input(int argc, char **argv);
 int	arg_is_digit(char *str);
 size_t	get_current_time(void);
+void	destroy_mutexes(t_program *prog);
+void	free_philos_and_forks(t_program *prog);
+int	create_join_threads(t_program *prog);
+void	wait_threads_creation(t_program *prog);
 
 #endif
